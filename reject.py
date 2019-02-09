d9b33488e5f48eafcecb5ef47ccdeb4205255c41
yinmo-public <yinmo.contact@gmail.com>
2019-02-09 20:35:10 +0800

    Delete reject.py

--- reject.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from linepy import *
-yinmo = LINE ()
-oepoll = OEPoll(yinmo)
-for groups in yinmo.getGroupIdsInvited():
-    yinmo.rejectGroupInvitation(groups)
-    print("已拒絕群組邀請")
