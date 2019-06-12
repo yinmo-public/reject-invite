c4638814b702f7ab9d1d138ad71f59324bfe585d
yinmo-public <yinmo.contact@gmail.com>
2019-06-12 11:16:46 +0800

    Update reject-en.py

--- reject-en.py
+++ reject-en.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from linepy import *
+from time import sleep
 yinmo = LINE()
 int1 = len(yinmo.getGroupIdsInvited())
 if int1 == 0:
@@ -7,6 +8,7 @@ if int1 == 0:
 else:
     for groups in yinmo.getGroupIdsInvited():
         print("Reject " + yinmo.getGroup(groups).name)
+        sleep(0.5)
         yinmo.rejectGroupInvitation(groups)
     print("\nYou reject" + str(int1) + "groups invitation")
     
